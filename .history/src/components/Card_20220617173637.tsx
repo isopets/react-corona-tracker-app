6495abf560b7d34ee5dc151f949f8ad916f72c87
yuto isogai <jandp.0717@gmail.com>
2022-06-17 17:38:38 +0900

    card change tsx

---
+++ .history/src/components/Card_20220617173637.tsx
@@ -0,0 +1,34 @@
+interface SingleCountriesDataType {
+  Country: string;
+  NewConfirmed: number;
+  TotalConfirmed: number;
+}
+
+interface CardType {
+  // 修正
+  allCountriesData: Array<SingleCountriesDataType>;
+}
+
+const Card = ({allCountriesData}) => {
+  return (
+    <div className="card-container">
+      {allCountriesData.map((singleData, index) => (
+        <div key={index} className="card">
+          <div>
+            <h2>{singleData.Country}</h2>
+            <p>
+              新規感染者:
+              <span>{singleData.NewConfirmed.toLocaleString()}</span>
+            </p>
+            <p>
+              感染者総数:
+              <span>{singleData.TotalConfirmed.toLocaleString()}</span>
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Card;
