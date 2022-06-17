8b58b5ae67abe50568853f36bbf9f517567959f1
yuto isogai <jandp.0717@gmail.com>
2022-06-17 14:19:40 +0900

    add _redirects file

--- .history/src/App_20220617141549.js
+++ .history/src/App_20220617141549.js
@@ -54,4 +54,3 @@ function App() {
     </BrowserRouter>
   );
 }
-export default App;
--- .history/src/components/Card_20220617141702.js
+++ .history/src/components/Card_20220617141702.js
@@ -6,11 +6,11 @@ const Card = props => {
           <div>
             <h2>{singleData.Country}</h2>
             <p>
-              新規感染者:
+              新規感染者:{" "}
               <span>{singleData.NewConfirmed.toLocaleString()}</span>
             </p>
             <p>
-              感染者総数:
+              感染者総数:{" "}
               <span>{singleData.TotalConfirmed.toLocaleString()}</span>
             </p>
           </div>
